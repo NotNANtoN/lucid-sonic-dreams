eaea5bdfd5808b51835345087c0cce0d97a440c4
Anton <antonwiehe@gmail.com>
2021-11-25 19:54:14 +0100

    Add new GPT prompting that uses CLIP on the fly

--- mustovi_utils.py
+++ mustovi_utils.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import musicnn
 from musicnn.extractor import extractor
 from musicnn.tagger import top_tags
@@ -5,6 +7,8 @@ import pandas as pd
 import numpy as np
 import librosa
 import torch
+import pandas as pd
+from clip import tokenize
 #remove deprecation warnings
 #import tensorflow as tf
 #tf.logging.set_verbosity(tf.logging.ERROR)
@@ -89,3 +93,266 @@ def slerp(low, high, val):
     so = torch.sin(omega)
     res = (torch.sin((1.0 - val) * omega) / so).unsqueeze(1) * low + (torch.sin(val * omega) / so).unsqueeze(1) * high
     return res
+
+
+def load_gpt_model(name):
+    from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+    device = torch.device("cuda")
+    if name == "neo1.3":
+        gpt_tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+        gpt_model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B", pad_token_id=gpt_tokenizer.eos_token_id)
+    elif name == "neo2.7":
+        gpt_tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+        gpt_model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-2.7B", pad_token_id=gpt_tokenizer.eos_token_id)
+    elif name == "gpt2":
+        from transformers import GPT2LMHeadModel
+        gpt_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        gpt_model = GPT2LMHeadModel.from_pretrained('gpt2', pad_token_id=gpt_tokenizer.eos_token_id)
+    gpt_model = gpt_model.to(device)
+    return gpt_model, gpt_tokenizer
+
+
+def gen_sent(gpt_model, gpt_tokenizer, clip_model, target_clip_feats, 
+             start_text="", p=0.9,
+             prefix="", examples=None, prompter=None, target_text=None,
+             clip_weight=0.4, 
+             clip_temp=0.45, gpt_temp=0.75, out_len=50, v=0, num_beams=20, return_num=1):
+    
+    context_text = prefix
+    if examples is not None:
+        # add examples to prompt
+        for example_input in examples:
+            example_target = examples[example_input]
+            context_text += f"{example_input}{prompter}{example_target}\n "
+        # add line to prompt actual output
+        context_text += f"{target_text}{prompter}"   
+    print("Context: ", "\n", context_text)
+    
+    data_dict = defaultdict(list)
+    for _ in range(num_beams):
+        sent, scores, clip_scores, gpt_scores = sample_GPT_with_CLIP(gpt_model, gpt_tokenizer, clip_model, target_clip_feats,
+                         start_text=start_text,
+                         p=p, 
+                         context_text=context_text,
+                         clip_weight=clip_weight,
+                         clip_temp=clip_temp,
+                         gpt_temp=gpt_temp,
+                         out_len=out_len,
+                         v=v,
+                        )
+        if v > -1:
+            #print("Scores:")
+            #print(scores.round(2))
+            print(clip_scores.round(2))
+            #print(gpt_scores.round(2))
+            print("During scores:", scores.mean().round(2))
+            print("Post scores:", (clip_scores[gpt_scores < 0.95].mean() + gpt_scores[gpt_scores < 0.95].mean()) / 2)
+            print("Post score weighted:", (clip_scores[gpt_scores < 0.95].mean()  * (clip_weight)+ gpt_scores[gpt_scores < 0.95].mean() * (1-clip_weight)))
+            print(clip_scores.mean().round(2))
+            print(gpt_scores.mean().round(2))
+            print("Sent:", sent)
+            #print(scores.mean() * np.sqrt(len(scores)))
+            print()
+        data_dict["sent"].append(sent)
+        data_dict["during_score"].append(scores.mean())
+        data_dict["post_score"].append((clip_scores[gpt_scores < 0.95].mean()  * (clip_weight)+ gpt_scores[gpt_scores < 0.95].mean() * (1-clip_weight)))
+        data_dict["clip_score"].append(clip_scores.mean())
+        data_dict["gpt_score"].append(gpt_scores.mean())
+    
+    df = pd.DataFrame(data_dict)
+    
+    
+    best_sentence = select_best_beam(df, clip_model, target_clip_feats, examples=examples, return_num=return_num)
+    
+    return best_sentence
+
+
+def norm(a):
+    return a / a.norm(dim=-1, keepdim=True)
+
+
+def select_best_beam(df, clip_model, target_clip_feats, examples=None, return_num=1,
+                     target_sim_thresh=0.93, example_sim_thresh=0.89, dir_sim_thresh=0.3):
+    # filter out sentences that just imitate the target text
+    out_encodings = clip_model.encode_text(tokenize(list(df["sent"])).to("cuda"))
+    similarity_score = torch.cosine_similarity(target_clip_feats, out_encodings)
+    df["target_similarity"] = similarity_score.tolist()
+
+    # filter out sentences that copy a given example
+    if examples is not None:
+        example_target_encodings = clip_model.encode_text(tokenize(list(examples.values())).to("cuda"))
+        similarity_score = [torch.cosine_similarity(out_encoding.unsqueeze(0), example_target_encodings).max().item()
+                            for out_encoding in out_encodings]
+        df["example_similarity"] = similarity_score
+
+        example_input_encodings = clip_model.encode_text(tokenize(list(examples.keys())).to("cuda"))
+        # get directions
+        directions = norm(norm(example_target_encodings) - norm(example_input_encodings))
+        mean_dir = norm(directions.mean(dim=0, keepdim=True))
+        # get similarity of predicted directions and mean training direction
+        generated_dirs = norm(norm(out_encodings) - norm(target_clip_feats))
+        dir_sims = torch.cosine_similarity(generated_dirs, mean_dir).cpu().tolist()
+        df["direction_similarity"] = dir_sims
+
+    # apply filters as long as they do not delete all prompts
+    df = df[df["target_similarity"] < target_sim_thresh] if len(df[df["target_similarity"] < target_sim_thresh]) > 0 else df
+    if examples is not None:
+        df = df[df["example_similarity"] < example_sim_thresh] if len(df[df["example_similarity"] < example_sim_thresh]) > 0 else df
+        df = df[df["direction_similarity"] > dir_sim_thresh] if len(df[df["direction_similarity"] > dir_sim_thresh]) > 0 else df
+
+    best_sentence = df.sort_values("post_score", ascending=False).iloc[:return_num]["sent"].to_list()
+    best_sentence = [s.strip() for s in best_sentence]
+    return best_sentence
+
+
+@torch.inference_mode()
+def sample_GPT_with_CLIP(model, tokenizer, clip_model, target_clip_feats,
+                         start_text="This", 
+                         context_text="",
+                         p=0.55, # top-p for GPT
+                         k=200, # top-k for GPT
+                         clip_temp=0.001, 
+                         gpt_temp=0.7,
+                         clip_weight=1.0,
+                         out_len=100,
+                         clip_bs=8,
+                         max_tokens=77,
+                         v=False,
+                         forbidden_words=";\/{}()[]|<>*+~#'-~",
+                        ):
+    
+    forbidden_tokens = [tokenizer.encode(w)[0] for w in forbidden_words]
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    context_tokens = tokenizer.encode(context_text)
+    generated = tokenizer.encode(context_text + start_text) if len(context_tokens) > 0 else [0]
+    context = torch.tensor([generated]).to(device)
+    past = None
+
+    num_choices_list = []
+    losses = []
+
+    scores = []
+    clip_scores = []
+    gpt_scores = []
+    num_tokens = len(tokenizer.encode(start_text)) if len(start_text) > 0 else 0
+    for i in range(out_len):
+        output = model(context, past_key_values=past, use_cache=True)
+        logits = output["logits"].squeeze(0)
+        if logits.ndim > 1:
+            logits = logits[-1]  # at first step logits for all tokens are calculated
+        past = output["past_key_values"]  # to cache computations
+
+        # choose token based on CLIP
+
+        # create sentence options
+        # filter out some tokens
+        #top_tokens = torch.topk(logits[..., -1, :], k, dim=-1).indices.squeeze()  # top k
+        logits = torch.softmax(logits / gpt_temp, dim=-1)
+        logits[forbidden_tokens] = 0
+        # top-p sampling
+        sorted_logits = torch.sort(logits, descending=True)
+        sorted_vals, sorted_idcs = sorted_logits.values[:k], sorted_logits.indices[:k]
+        cumsum = torch.cumsum(sorted_vals, dim=0)
+        mask = cumsum < p
+        if mask[0] == 0:
+            mask[0] = 1  # include at least one token
+        top_tokens = sorted_idcs[mask]
+        gpt_top_logits = sorted_vals[mask]
+
+
+        top_generations = [generated[len(context_tokens):] + [token.item()] for token in top_tokens]
+        top_sentences = [tokenizer.decode(gen) for gen in top_generations]
+
+        if v > 1:
+            print("Num sentences: ", len(top_sentences))
+            print("Some top sentences: ", top_sentences[:10])
+        
+        # calc CLIP sentence-img similarity scores
+        all_feats = []
+        for i in range(0, len(top_sentences), clip_bs):
+            sentence_batch = top_sentences[i: i + clip_bs]
+            clip_encoded = tokenize(sentence_batch).to(device) #torch.cat([tokenize(sentence_batch) for sentence in sentence_batch]).to(device)
+            clip_sentence_feats = clip_model.encode_text(clip_encoded)
+            all_feats.extend([f for f in clip_sentence_feats])
+        all_feats = torch.stack(all_feats)
+        clip_similarities = torch.nn.functional.cosine_similarity(all_feats, target_clip_feats, dim=-1)
+        std = clip_similarities.std()
+        if std > 0:
+            normed_clip_similarities = (clip_similarities - clip_similarities.mean()) / std
+        else:
+            normed_clip_similarities = clip_similarities
+        clip_similarities_softmax = torch.softmax(normed_clip_similarities / clip_temp, dim=0)
+
+        #print(torch.topk(clip_similarities, min(10, len(clip_similarities)), dim=-1).values.tolist())  # top k
+
+        # choose best fitting token
+        #token = top_tokens[torch.argmax(clip_similarities)]  # greedy
+        if v > 1:
+            print("First 10 gpt logits. ", np.round(gpt_top_logits[:10].cpu().numpy(), 2))
+            print("First 10 clip logits. ", np.round(clip_similarities_softmax[:10].cpu().numpy(), 2))
+        weights = gpt_top_logits * (1 - clip_weight) + clip_similarities_softmax * clip_weight
+        if v > 1:
+            print("First 10 weights. ", weights[:10])
+        idx = torch.multinomial(weights, num_samples=1)
+        token = top_tokens[idx][0]
+        scores.append(weights[idx].item())
+        clip_scores.append(clip_similarities[idx].item())
+        gpt_scores.append(gpt_top_logits[idx].item())
+
+
+        #token = torch.argmax(logits[..., -1, :])    
+        generated += [token.tolist()]
+        context = token.unsqueeze(0)
+        
+        token_decoded = tokenizer.decode(token.tolist())
+        if v > 0:
+            print(token_decoded)
+            
+        num_tokens += 1
+        
+        if max_tokens is not None and num_tokens >= max_tokens:
+            break
+        if token_decoded == "\n":
+            break
+
+        num_choices_list.append(len(top_tokens))
+        losses.append(clip_similarities[idx].detach().cpu().item())
+        #pbar.set_description(f"{tokenizer.decode(generated)} - {len(top_tokens)}")
+
+    sequence = tokenizer.decode(generated[len(context_tokens):])
+    return sequence, np.array(scores), np.array(clip_scores), np.array(gpt_scores)
+
+
+
+import torch
+
+def gpt_create_prompt(model, tokenizer, merged_top_tags, num_beams=6, top_p=0.80, prompt_num=1):
+    if prompt_num == 1:
+        prompter = ". Full description:"
+        gpt_prompt = f"""The following are adjectives describing a song, followed by a description of the corresponding image:
+    fast, sad, dark{prompter} A man running through dark woods while crying.
+    loud, techno, electronic, abstract{prompter} Dynamic and vibrant colors forming strong geometric shapes that give the impression of a rave.
+    slow, soft, beautiful, sad, quiet{prompter} An old woman who is sitting on a chair with her hands folded in front of her. She is looking at you with a sad expression on her face.
+    {merged_top_tags}{prompter}"""
+    if prompt_num == 2:
+        prompter = ". Full description:"
+        gpt_prompt = f"""The following are adjectives describing an image, followed by a full description of the corresponding image:
+    fast, sad, dark{prompter} A man is running through dark woods while crying.
+    loud, techno, electronic, abstract{prompter} Dynamic and vibrant colors forming strong geometric shapes that give the impression of a rave.
+    slow, soft, beautiful, sad, quiet{prompter} An old woman is sitting on a chair in a beautiful garden with her hands folded in front of her. She is looking at you with a sad expression on her face.
+    {merged_top_tags}{prompter}"""
+    
+    out = model.generate(tokenizer.encode(gpt_prompt, return_tensors="pt").to(model.device),
+                         top_p=top_p, do_sample=True, output_scores=True, 
+                         return_dict_in_generate=True, 
+                         max_length=220,
+                         num_beams=num_beams, 
+                         no_repeat_ngram_size=2,)
+    out_text = tokenizer.decode(out["sequences"].tolist()[0])
+    #print(out_text.split("Corresponding image description:")[3:])
+    clip_prompt = out_text.split(prompter)[-1].split("\n")[0].strip().strip('<|endoftext|>')
+    if not clip_prompt.endswith("."):
+        clip_prompt = ".".join(clip_prompt.split(".")[:-1]) + "."
+    return clip_prompt, out_text
